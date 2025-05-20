3dd5ced085a0c25cb0bb85465a6eb69d56afad97
Mykola Kizan <mkizan@gmail.com>
2025-05-20 21:03:20 +0300

    add CRUD operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+    NgFor,
+    NgIf,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'firstAngularApplication';
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectedUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
+
+@Component({
+  selector: 'app-user',
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectedUser() {
+    this.select.emit(this.user.id);
+  }
+}
